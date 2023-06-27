cf0638afaa1c7d5f01b911bdde7b3d9a22790e49
mastruzz <mateus.gtn.oliveira@gmail.com>
2023-06-26 21:09:57 -0300

    Corrigindo ASCII

--- src/section02/SmileyFace.java
+++ src/section02/SmileyFace.java
@@ -3,13 +3,13 @@ package section02;
 public class SmileyFace {
 
     public static void main(String[] args) {
-        System.out.println("    ************                                ");
-        System.out.println("  **            **                               ");
-        System.out.println("**     *   *     **                              ");
-        System.out.println("**               **                            ");
-        System.out.println("**   **      **  **                            ");
-        System.out.println(" **    ******    **                        ");
-        System.out.println("   **          **                               ");
-        System.out.println("     ***********                                 ");
+        System.out.println("                          ************                                ");
+        System.out.println("                       **             **                               ");
+        System.out.println("                     **      *   *       **                              ");
+        System.out.println("                     **                  **                            ");
+        System.out.println("                     **   **       **    **                            ");
+        System.out.println("                      **    *******     **                        ");
+        System.out.println("                        **            **                               ");
+        System.out.println("                          ***********                                 ");
     }
 }
